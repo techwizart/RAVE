1c00438f7ea7308759125d99765c2c41b44aa1f7
Andrei <amakhtev@quantumbrains.com>
2026-05-05 03:01:36 +0400

    fix typo

--- rave/blocks.py
+++ rave/blocks.py
@@ -6,7 +6,7 @@ import gin
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn.utils.parametrize import weight_norm
+from torch.nn.utils.parametrizations import weight_norm
 
 from torchaudio.transforms import Spectrogram
 
