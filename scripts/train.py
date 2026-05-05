a36d8b483331cf95bc7654b2d7bf3e3fdd03f111
Andrei <amakhtev@quantumbrains.com>
2026-05-05 06:57:09 +0400

    pin memory and prefetch for dataloader worker

--- scripts/train.py
+++ scripts/train.py
@@ -12,8 +12,9 @@ from torch.utils.data import DataLoader
 try:
     import rave
 except:
-    import sys, os 
-    sys.path.append(os.path.abspath('.'))
+    import sys, os
+
+    sys.path.append(os.path.abspath("."))
     import rave
 
 import rave
@@ -24,76 +25,56 @@ from rave.transforms import get_augmentations, add_augmentation
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('name', None, help='Name of the run', required=True)
-flags.DEFINE_multi_string('config',
-                          default='v2.gin',
-                          help='RAVE configuration to use')
-flags.DEFINE_multi_string('augment',
-                           default = [],
-                            help = 'augmentation configurations to use')
-flags.DEFINE_string('db_path',
-                    None,
-                    help='Preprocessed dataset path',
-                    required=True)
-flags.DEFINE_string('out_path',
-                    default="runs/",
-                    help='Output folder')
-flags.DEFINE_integer('max_steps',
-                     6000000,
-                     help='Maximum number of training steps')
-flags.DEFINE_integer('val_every', 10000, help='Checkpoint model every n steps')
-flags.DEFINE_integer('save_every',
-                     500000,
-                     help='save every n steps (default: just last)')
-flags.DEFINE_integer('n_signal',
-                     131072,
-                     help='Number of audio samples to use during training')
-flags.DEFINE_integer('channels', 0, help="number of audio channels")
-flags.DEFINE_integer('batch', 8, help='Batch size')
-flags.DEFINE_string('ckpt',
-                    None,
-                    help='Path to previous checkpoint of the run')
-flags.DEFINE_multi_string('override', default=[], help='Override gin binding')
-flags.DEFINE_integer('workers',
-                     default=8,
-                     help='Number of workers to spawn for dataset loading')
-flags.DEFINE_multi_integer('gpu', default=None, help='GPU to use')
-flags.DEFINE_bool('derivative',
-                  default=False,
-                  help='Train RAVE on the derivative of the signal')
-flags.DEFINE_bool('normalize',
-                  default=False,
-                  help='Train RAVE on normalized signals')
-flags.DEFINE_list('rand_pitch',
-                  default=None,
-                  help='activates random pitch')
-flags.DEFINE_float('ema',
-                   default=None,
-                   help='Exponential weight averaging factor (optional)')
-flags.DEFINE_bool('progress',
-                  default=True,
-                  help='Display training progress bar')
-flags.DEFINE_bool('smoke_test', 
-                  default=False,
-                  help="Run training with n_batches=1 to test the model")
+flags.DEFINE_string("name", None, help="Name of the run", required=True)
+flags.DEFINE_multi_string("config", default="v2.gin", help="RAVE configuration to use")
+flags.DEFINE_multi_string(
+    "augment", default=[], help="augmentation configurations to use"
+)
+flags.DEFINE_string("db_path", None, help="Preprocessed dataset path", required=True)
+flags.DEFINE_string("out_path", default="runs/", help="Output folder")
+flags.DEFINE_integer("max_steps", 6000000, help="Maximum number of training steps")
+flags.DEFINE_integer("val_every", 10000, help="Checkpoint model every n steps")
+flags.DEFINE_integer(
+    "save_every", 500000, help="save every n steps (default: just last)"
+)
+flags.DEFINE_integer(
+    "n_signal", 131072, help="Number of audio samples to use during training"
+)
+flags.DEFINE_integer("channels", 0, help="number of audio channels")
+flags.DEFINE_integer("batch", 8, help="Batch size")
+flags.DEFINE_string("ckpt", None, help="Path to previous checkpoint of the run")
+flags.DEFINE_multi_string("override", default=[], help="Override gin binding")
+flags.DEFINE_integer(
+    "workers", default=8, help="Number of workers to spawn for dataset loading"
+)
+flags.DEFINE_multi_integer("gpu", default=None, help="GPU to use")
+flags.DEFINE_bool(
+    "derivative", default=False, help="Train RAVE on the derivative of the signal"
+)
+flags.DEFINE_bool("normalize", default=False, help="Train RAVE on normalized signals")
+flags.DEFINE_list("rand_pitch", default=None, help="activates random pitch")
+flags.DEFINE_float(
+    "ema", default=None, help="Exponential weight averaging factor (optional)"
+)
+flags.DEFINE_bool("progress", default=True, help="Display training progress bar")
+flags.DEFINE_bool(
+    "smoke_test", default=False, help="Run training with n_batches=1 to test the model"
+)
 
 
 class EMA(pl.Callback):
-
-    def __init__(self, factor=.999) -> None:
+    def __init__(self, factor=0.999) -> None:
         super().__init__()
         self.weights = {}
         self.factor = factor
 
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch,
-                           batch_idx) -> None:
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx) -> None:
         for n, p in pl_module.named_parameters():
             if n not in self.weights:
                 self.weights[n] = p.data.clone()
                 continue
 
-            self.weights[n] = self.weights[n] * self.factor + p.data * (
-                1 - self.factor)
+            self.weights[n] = self.weights[n] * self.factor + p.data * (1 - self.factor)
 
     def swap_weights(self, module):
         for n, p in module.named_parameters():
@@ -119,10 +100,12 @@ class EMA(pl.Callback):
     def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
         self.weights.update(state_dict)
 
+
 def add_gin_extension(config_name: str) -> str:
-    if config_name[-4:] != '.gin':
-        config_name += '.gin'
+    if config_name[-4:] != ".gin":
+        config_name += ".gin"
     return config_name
+
 
 def parse_augmentations(augmentations):
     for a in augmentations:
@@ -131,23 +114,24 @@ def parse_augmentations(augmentations):
         gin.clear_config()
     return get_augmentations()
 
+
 def main(argv):
-    torch.set_float32_matmul_precision('high')
+    torch.set_float32_matmul_precision("high")
     torch.backends.cudnn.benchmark = True
 
     # check dataset channels
     n_channels = rave.dataset.get_training_channels(FLAGS.db_path, FLAGS.channels)
-    gin.bind_parameter('RAVE.n_channels', n_channels)
+    gin.bind_parameter("RAVE.n_channels", n_channels)
 
     # parse augmentations
     augmentations = parse_augmentations(map(add_gin_extension, FLAGS.augment))
-    gin.bind_parameter('dataset.get_dataset.augmentations', augmentations)
+    gin.bind_parameter("dataset.get_dataset.augmentations", augmentations)
 
     # parse configuration
     if FLAGS.ckpt:
         config_file = rave.core.search_for_config(FLAGS.ckpt)
         if config_file is None:
-            print('Config file not found in %s'%FLAGS.run)
+            print("Config file not found in %s" % FLAGS.run)
         gin.parse_config_file(config_file)
     else:
         gin.parse_config_files_and_bindings(
@@ -161,31 +145,47 @@ def main(argv):
         model.integrator = rave.dataset.get_derivator_integrator(model.sr)[1]
 
     # parse datasset
-    dataset = rave.dataset.get_dataset(FLAGS.db_path,
-                                       model.sr,
-                                       FLAGS.n_signal,
-                                       derivative=FLAGS.derivative,
-                                       normalize=FLAGS.normalize,
-                                       rand_pitch=FLAGS.rand_pitch,
-                                       n_channels=n_channels)
+    dataset = rave.dataset.get_dataset(
+        FLAGS.db_path,
+        model.sr,
+        FLAGS.n_signal,
+        derivative=FLAGS.derivative,
+        normalize=FLAGS.normalize,
+        rand_pitch=FLAGS.rand_pitch,
+        n_channels=n_channels,
+    )
     train, val = rave.dataset.split_dataset(dataset, 98)
 
     # get data-loader
     num_workers = FLAGS.workers
     if os.name == "nt" or sys.platform == "darwin":
         num_workers = 0
-    train = DataLoader(train,
-                       FLAGS.batch,
-                       True,
-                       drop_last=True,
-                       num_workers=num_workers)
-    val = DataLoader(val, FLAGS.batch, False, num_workers=num_workers)
+    train = DataLoader(
+        train,
+        FLAGS.batch,
+        True,
+        drop_last=True,
+        pin_memory=True,
+        prefetch_factor=4,
+        num_workers=num_workers,
+    )
+    val = DataLoader(
+        val,
+        FLAGS.batch,
+        False,
+        num_workers=num_workers,
+        pin_memory=True,
+        prefetch_factor=4,
+    )
 
     # CHECKPOINT CALLBACKS
-    validation_checkpoint = pl.callbacks.ModelCheckpoint(monitor="validation",
-                                                         filename="best")
-    last_filename = "last" if FLAGS.save_every is None else "epoch-{epoch:04d}"                                                        
-    last_checkpoint = rave.core.ModelCheckpoint(filename=last_filename, step_period=FLAGS.save_every)
+    validation_checkpoint = pl.callbacks.ModelCheckpoint(
+        monitor="validation", filename="best"
+    )
+    last_filename = "last" if FLAGS.save_every is None else "epoch-{epoch:04d}"
+    last_checkpoint = rave.core.ModelCheckpoint(
+        filename=last_filename, step_period=FLAGS.save_every
+    )
 
     val_check = {}
     if len(train) >= FLAGS.val_every:
@@ -195,13 +195,12 @@ def main(argv):
         val_check["check_val_every_n_epoch"] = nepoch
 
     if FLAGS.smoke_test:
-        val_check['limit_train_batches'] = 1
-        val_check['limit_val_batches'] = 1
+        val_check["limit_train_batches"] = 1
+        val_check["limit_val_batches"] = 1
 
-    gin_hash = hashlib.md5(
-        gin.operative_config_str().encode()).hexdigest()[:10]
+    gin_hash = hashlib.md5(gin.operative_config_str().encode()).hexdigest()[:10]
 
-    RUN_NAME = f'{FLAGS.name}_{gin_hash}'
+    RUN_NAME = f"{FLAGS.name}_{gin_hash}"
 
     os.makedirs(os.path.join(FLAGS.out_path, RUN_NAME), exist_ok=True)
 
@@ -210,7 +209,7 @@ def main(argv):
     else:
         gpu = FLAGS.gpu or rave.core.setup_gpu()
 
-    print('selected gpu:', gpu)
+    print("selected gpu:", gpu)
 
     accelerator = None
     devices = None
@@ -256,17 +255,17 @@ def main(argv):
 
     run = rave.core.search_for_run(FLAGS.ckpt)
     if run is not None:
-        print('loading state from file %s'%run)
-        loaded = torch.load(run, map_location='cpu')
+        print("loading state from file %s" % run)
+        loaded = torch.load(run, map_location="cpu")
         # model = model.load_state_dict(loaded)
-        trainer.fit_loop.epoch_loop._batches_that_stepped = loaded['global_step']
+        trainer.fit_loop.epoch_loop._batches_that_stepped = loaded["global_step"]
         # model = model.load_state_dict(loaded['state_dict'])
-    
+
     with open(os.path.join(FLAGS.out_path, RUN_NAME, "config.gin"), "w") as config_out:
         config_out.write(gin.operative_config_str())
 
     trainer.fit(model, train, val, ckpt_path=run)
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(main)
